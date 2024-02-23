444afc4b02c8280b9ec6bb3f054595aba3a79845
Michel Castro <42982212+mixchel@users.noreply.github.com>
2024-02-23 19:29:49 +0000

    made check_win_after_move

--- connect4oop.py
+++ connect4oop.py
@@ -6,6 +6,7 @@ EMPTY = '-'
 PLAYER_PIECE = 'X'
 
 class game:
+    game_winner = EMPTY
     def __init__(self):
         self.board = np.full([NUM_ROW,NUM_COL],EMPTY)
 
@@ -14,18 +15,20 @@ class game:
         return
     
     def playOneTurn(self):
-        self.drawBoard()
-        available = game.availableRows()
+        available = self.availableRows()
         collumn = int(input("Choose in which collumn do you wanna play"))
         while collumn not in available:
             collumn = int(input("The collumn you selected is either full or invalid, choose another one"))
-            print(collumn)
-        game.putGamePiece(collumn, PLAYER_PIECE)
+        self.putGamePiece(collumn, PLAYER_PIECE)
         return
     
     def putGamePiece(self, collumn, piece):
-        self.board[self.nextEmptyRowinCollumn(collumn)][collumn] = piece
-        return
+        piece_placement = self.nextEmptyRowinCollumn(collumn)
+        self.board[piece_placement][collumn] = piece
+        if self.check_win_after_move(piece_placement, collumn, piece): game_winner = piece
+             
+        
+
     
     def availableRows(self):
         available = []
@@ -36,6 +39,36 @@ class game:
     def nextEmptyRowinCollumn(self, collumn):
         for i,value in enumerate(self.board[:,collumn]):
             if value == EMPTY: return i
-    def checkWin(self):
-        pass
-        
+
+    # def segments(self):
+    #     segmentsList = []
+    #     for row in self.board:
+    #         segmentsList.append(row)
+    #     # Coloca as linhas da matriz na lista
+    #     for row in np.transpose(self.board):
+    #         segmentsList.append(row)
+    #     # Coloca as linhas da transposta da matriz na lista, equivalente as colunas da matriz original
+    def check_win_after_move(self, moverow, movecollumn, piece):
+        for i in range(moverow - 3,moverow + 4):
+            if i in range(0,6) and self.board[i][movecollumn] == piece: count += 1
+            else: count = 0
+        if count >= 4: return True
+        count = 0
+        for j in range(movecollumn - 3, movecollumn + 4):
+            if j in range(0,7) and self.board[moverow][j] == piece: count += 1
+            else: count = 0  
+        if count >= 4: return True
+        for k in range(-3, 4):
+            i = moverow + k
+            j = movecollumn + k 
+            if i in range(0,6) and j in range(0,6) and self.board[i][j] == piece: count += 1
+            else: count = 0
+        if count >= 4: return True
+        for k in range(-3, 4):
+            i = moverow - k
+            j = movecollumn + k 
+            if i in range(0,6) and j in range(0,6) and self.board[i][j] == piece: count += 1
+            else: count = 0
+        if count >= 4: return True
+        return False
+    
