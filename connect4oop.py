cb133e9f6e2452cc53fa994e35102d0100f6ff42
Michel Castro <42982212+mixchel@users.noreply.github.com>
2024-02-23 17:33:34 +0000

    Changes to OOP

--- connect4oop.py
+++ connect4oop.py
@@ -2,24 +2,40 @@ import numpy as np
 
 NUM_ROW = 6
 NUM_COL = 7
-EMPTY = "-"
+EMPTY = '-'
+PLAYER_PIECE = 'X'
 
 class game:
-    turn = 0
     def __init__(self):
         self.board = np.full([NUM_ROW,NUM_COL],EMPTY)
-    def printBoard(self):
-        print(self.board)
+
+    def drawBoard(self):
+        print(np.flip(self.board, 0))
+        return
+    
     def playOneTurn(self):
-        self.printBoard()
+        self.drawBoard()
         available = game.availableRows()
-        row = int(input("Choose in which collumn do you wanna play"))
-        while row not in available:
-            row = input("The collumn you selected is either full or invalid, choose another one")
-    def putGamePiece(self, collumn):
-        pass
+        collumn = int(input("Choose in which collumn do you wanna play"))
+        while collumn not in available:
+            collumn = int(input("The collumn you selected is either full or invalid, choose another one"))
+            print(collumn)
+        game.putGamePiece(collumn, PLAYER_PIECE)
+        return
+    
+    def putGamePiece(self, collumn, piece):
+        self.board[self.nextEmptyRowinCollumn(collumn)][collumn] = piece
+        return
+    
     def availableRows(self):
         available = []
-        for i,value in enumerate(self.board[0]):
+        for i,value in enumerate(self.board[5]):
             if value == EMPTY: available.append(i) 
         return available
+    
+    def nextEmptyRowinCollumn(self, collumn):
+        for i,value in enumerate(self.board[:,collumn]):
+            if value == EMPTY: return i
+    def checkWin(self):
+        pass
+        
