11fa18c7aee4fb02151b171cb02975d230728ac4
Diogo Cruz <up202005947@edu.med.up.pt>
2024-02-29 16:55:03 +0000

    Simple A+ implementation

--- aStar.py
+++ aStar.py
@@ -2,6 +2,8 @@
 #classes em Java era mais fácil....
 
 from game import *
+import copy
+import numpy as np
 class aStar:
     def __init__(self) -> None:
         pass
@@ -10,6 +12,16 @@ class aStar:
     nesse caso, se a primeira coluna não tiver vazia ele retorna 6,
     o que quer dizer "por um x na ultima coluna"
     """
-    def test_move(self,board_state):
-        if board_state[0][1] != EMPTY:
-            return 6
+    def get_move(self, state):
+        actions = []
+        heuristic = []
+        for action in state.availableCollumns():
+            new_state = copy.deepcopy(state)
+            new_state.putGamePiece(action, "X")
+            actions.append(action)
+            heuristic.append(new_state.evaluate_all())
+        index_min = np.argmin(heuristic)
+        print(heuristic, actions)
+        return actions[index_min]
+
+
